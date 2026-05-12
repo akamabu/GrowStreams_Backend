bf89f46f4b0c0f22efbc279fb8b68f732acf29b1
Nguyễn Phương <55752553+akamabu@users.noreply.github.com>
2026-05-12 22:28:00 +0700

    Frontend: Add JSDoc to utils

--- frontend/lib/utils.ts
+++ frontend/lib/utils.ts
@@ -1,6 +1,7 @@
 import { clsx, type ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
 
+/** Combined classes utility */
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
