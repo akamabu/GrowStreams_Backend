4c9f4a1288ab0ab53d8df6c5a8b83374b70e50c0
Nguyễn Phương <55752553+akamabu@users.noreply.github.com>
2026-05-12 23:25:55 +0700

    Docs: Document bounty.mjs

--- api/src/routes/bounty.mjs
+++ api/src/routes/bounty.mjs
@@ -1,3 +1,4 @@
+/** Documents api/src/routes/bounty.mjs module purpose and usage context */
 import { Router } from 'express';
 import { query, command, encodePayload } from '../sails-client.mjs';
 
